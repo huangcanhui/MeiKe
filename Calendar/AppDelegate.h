f539930e79af895ded9500e5497eb796bb1eace9
huangcanhui <HCH@CHHuangdeiMac.local>
2017-11-06 11:38:08 +0800

    Initial Commit

---
+++ Calendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calendar
+//
+//  Created by huangcanhui on 2017/11/6.
+//  Copyright © 2017年 HuangCanHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
