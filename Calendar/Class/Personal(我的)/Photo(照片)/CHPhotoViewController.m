cd5fb805f8d78472ad9608f6aa3d1867cdc78a78
huangcanhui <HCH@CHHuangdeiMac.local>
2017-11-30 19:47:37 +0800

    相册功能

--- Calendar/Class/Personal(我的)/Photo(照片)/CHPhotoViewController.m
+++ Calendar/Class/Personal(我的)/Photo(照片)/CHPhotoViewController.m
@@ -8,16 +8,99 @@
 
 #import "CHPhotoViewController.h"
 
-@interface CHPhotoViewController ()
-
+@interface CHPhotoViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+/**
+ * UICollectionView
+ */
+@property (nonatomic, strong)UICollectionView *collectionView;
+@property (nonatomic, strong)UICollectionViewFlowLayout *flowLayout;
 @end
 
+static NSString *bundleID = @"photo";
+static NSString *headerID = @"header";
+static CGFloat Maigin = 10.0f;
 @implementation CHPhotoViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = HexColor(0xffffff);
+    
+    self.navigationItem.title = @"我的照片";
+    
+    [self.view addSubview:self.collectionView];
 }
+
+- (UICollectionView *)collectionView
+{
+    if (!_collectionView) {
+        _collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:self.flowLayout];
+        _collectionView.backgroundColor = HexColor(0xffffff);
+        _collectionView.contentInset = UIEdgeInsetsMake(0, 0, navigationHeight, 0);
+        _collectionView.delegate = self;
+        _collectionView.dataSource = self;
+        //注册cell
+        [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:bundleID];
+        [_collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:headerID];
+    }
+    return _collectionView;
+}
+
+- (UICollectionViewFlowLayout *)flowLayout
+{
+    if (!_flowLayout) {
+        _flowLayout = [[UICollectionViewFlowLayout alloc] init];
+        CGFloat itemWidth = (kScreenWidth - 4 * Maigin) / 3;
+        //设置单元格大小
+        _flowLayout.itemSize = CGSizeMake(itemWidth, itemWidth);
+        //最小行间距
+        _flowLayout.minimumLineSpacing = 10;
+        //设置section的内边距
+        _flowLayout.sectionInset = UIEdgeInsetsMake(Maigin, Maigin, Maigin, Maigin);
+        //设置UICollectionView的滑动方向
+        _flowLayout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        //sectionheader的大小
+        _flowLayout.headerReferenceSize = CGSizeMake(0, 45);
+    }
+    return _flowLayout;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 5;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 15;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+//    UICollectionReusableView *view = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:headerID forIndexPath:indexPath];
+//    if (kind == UICollectionElementKindSectionHeader) {
+//        UIView *headerView = [[UIView alloc] initWithFrame:view.frame];
+//        headerView.backgroundColor = [UIColor blueColor];
+//        [view addSubview:headerView];
+//    }
+//    return view;
+    if ([kind isEqualToString:UICollectionElementKindSectionHeader]) {
+        UICollectionReusableView *header = [collectionView dequeueReusableSupplementaryViewOfKind:kind withReuseIdentifier:headerID forIndexPath:indexPath];
+        UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 45)];
+        headerView.backgroundColor = [UIColor blueColor];
+        [header addSubview:headerView];
+        return header;
+    }
+    return nil;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:bundleID forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor orangeColor];
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
